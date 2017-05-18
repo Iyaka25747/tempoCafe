a41aef4915db427d8bc16fc9557b3d5643dea205
ilya <ilya@ilya-THINK>
2017-05-18 22:27:16 +0200

    18/5/17 main board, button, Hex display, Relay Pin OK

--- TemporisateurCafe.ino
+++ TemporisateurCafe.ino
@@ -11,44 +11,61 @@ Hardware Pin declaration
 #define PIN_OUTPUT 1
 #define LOW 0
 #define HIGH 1
-#define RELAY_PIN 6 // Command of relay
-#define Button_PIN 5 // Button read
-#define DispClkPin 3 // TMS163 Display
-#define DispDataPin 2 // TMS163 Display
-// Improvement: Add potentiometer read to adjust timing
+int RELAY_PIN = 6;// Command of relay
+int Button_PIN = 5; // Button read
+int DispClkPin = 3; // TMS163 Display
+int DispDataPin = 2; // TMS163 Display
+// Improvement ?: Add potentiometer read to adjust timing
 
-const long duration;
+const long countDownDuration = 5000; //CountDown duration in millis
 
 /*
-HEX Display countdown
-*/
+ HEX Display countdown
+ */
 //Measured temperature
 const int CLK = DispClkPin; //Set the CLK pin connection to the display
 const int DIO1 = DispDataPin; //Set the DIO pin connection to the display
 TM1637Display displayCountDown(CLK, DIO1); //set up the 4-Digit Display1.
 
-
+int buttonState;
+unsigned long duration = 0;
+int i = 0;
+unsigned long  remainingTime;
 
 //The setup function is called once at startup of the sketch
-void setup()
-{
+void setup() {
 	pinMode(RELAY_PIN, PIN_OUTPUT);	// set relay pin to output
 	digitalWrite(RELAY_PIN, 0); //Set relay to OFF
-	pinMode(Button_PIN, PIN_INPUT);// set button pin to input
-	displayCountDown.setBrightness(0x0a);//set the display to maximum brightness
+	pinMode(Button_PIN, PIN_INPUT); // set button pin to input
+	displayCountDown.setBrightness(0x0a); //set the display to maximum brightness
 	Serial.begin(9600);			// initialize serial communications at 9600 bps:
-
+	digitalWrite(RELAY_PIN, LOW);
 }
 
 // The loop function is called in an endless loop
-void loop()
-{
+void loop() {
+	digitalWrite(RELAY_PIN, LOW);
+	buttonState = digitalRead(Button_PIN);
 
-/*If not button press do nothing
-If button press
-activate relay
-loog countdowm
-	Dispaly timing
-de-activate relay*/
+	// print out the state of the button:
+	Serial.println(buttonState);
+	displayCountDown.showNumberDec(countDownDuration);
+
+	if (buttonState == 1) {
+		Serial.println("starting the countdown");
+
+		unsigned long startTime = millis();
+		duration = 0;
+		i = 0;
+		do {
+			digitalWrite(RELAY_PIN, HIGH);
+			Serial.println("countdown is going on");
+			duration = millis() - startTime;
+			displayCountDown.showNumberDec(remainingTime);
+			remainingTime = countDownDuration - duration;
+		} while (duration < countDownDuration);
+
+	}
+
 
 }
